4487db383fc8b551cc7df2829ec9df4ebf593be2
Justin Harder <justinharder@t-online.de>
2020-07-20 10:51:24 +0200

    Teste KontaktService

--- src/test/java/de/justinharder/trainharder/model/services/KontaktServiceSollte.java
+++ src/test/java/de/justinharder/trainharder/model/services/KontaktServiceSollte.java
@@ -3,12 +3,19 @@ package de.justinharder.trainharder.model.services;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import de.justinharder.trainharder.model.services.mail.Mail;
+import de.justinharder.trainharder.model.services.mail.MailAdresse;
 import de.justinharder.trainharder.model.services.mail.MailServer;
+import de.justinharder.trainharder.view.dto.Kontaktformular;
 
 public class KontaktServiceSollte
 {
@@ -20,6 +27,7 @@ public class KontaktServiceSollte
 	public void setup()
 	{
 		mailServer = mock(MailServer.class);
+
 		sut = new KontaktService(mailServer);
 	}
 
@@ -32,5 +40,30 @@ public class KontaktServiceSollte
 		final var exception = assertThrows(NullPointerException.class, () -> sut.kontaktiere(null));
 
 		assertThat(exception.getMessage()).isEqualTo(erwartet);
+	}
+
+	@Test
+	@DisplayName("erfolgreich kontaktieren und Mail an MailServer weitergeben")
+	public void test02()
+	{
+		final var kontaktformular =
+			new Kontaktformular("mail@justinharder.de", "harder", "Justin", "Harder", "Ich habe ein Problem.");
+
+		sut.kontaktiere(kontaktformular);
+
+		verify(mailServer).sendeMail(new Mail(
+			new MailAdresse("mail@justinharder.de", "TrainHarder-Team"),
+			List.of(new MailAdresse("justinharder@t-online.de", "Justin Harder")),
+			List.of(),
+			List.of(),
+			"Support-Anfrage von " + kontaktformular.getBenutzername(),
+			"Eine Support-Anfrage von " + kontaktformular.getBenutzername()
+				+ "Benutzer:\n"
+				+ "\tBenutzername: " + kontaktformular.getBenutzername() + "\n"
+				+ "\tE-Mail-Adresse: " + kontaktformular.getMail() + "\n"
+				+ "\tName: " + kontaktformular.getVorname() + " " + kontaktformular.getNachname() + "\n"
+				+ "Nachricht:\n"
+				+ "\t" + kontaktformular.getNachricht()),
+			StandardCharsets.UTF_8);
 	}
 }
