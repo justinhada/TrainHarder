1fe2a30b4b51971049f18c9af594151bf3204bae
Justin Harder <justinharder@t-online.de>
2020-07-28 17:36:32 +0200

    Nutze nur relevante Daten

--- src/main/java/de/justinharder/trainharder/model/domain/Koerpermessung.java
+++ src/main/java/de/justinharder/trainharder/model/domain/Koerpermessung.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import de.justinharder.trainharder.model.domain.embeddables.Primaerschluessel;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -28,88 +29,60 @@ public class Koerpermessung extends Entitaet
 	@EmbeddedId
 	@Column(name = "ID")
 	private Primaerschluessel primaerschluessel;
+	@Column(name = "Datum")
+	private LocalDate datum;
 	@Column(name = "Koerpergroesse")
 	private int koerpergroesse;
 	@Column(name = "Koerpergewicht")
 	private double koerpergewicht;
-	@Column(name = "BodyMassIndex")
-	private double bodyMassIndex;
-	@Column(name = "FatFreeMassIndex")
-	private double fatFreeMassIndex;
 	@Column(name = "KoerperfettAnteil")
 	private double koerperfettAnteil;
-	@Column(name = "FettfreiesKoerpergewicht")
-	private double fettfreiesKoerpergewicht;
-	@Column(name = "SubkutanesFett")
-	private double subkutanesFett;
-	@Column(name = "Viszeralfett")
-	private double viszeralfett;
-	@Column(name = "Koerperwasser")
-	private double koerperwasser;
-	@Column(name = "Skelettmuskel")
-	private double skelettmuskel;
-	@Column(name = "Muskelmasse")
-	private double muskelmasse;
-	@Column(name = "Knochenmasse")
-	private double knochenmasse;
-	@Column(name = "Protein")
-	private double protein;
-	@Column(name = "Grundumsatz")
-	private int grundumsatz;
 	@Column(name = "EingenommeneKalorien")
 	private int eingenommeneKalorien;
 	@Column(name = "VerbrannteKalorien")
 	private int verbrannteKalorien;
-	@Column(name = "BiologischesAlter")
-	private int biologischesAlter;
-	@Column(name = "Datum")
-	private LocalDate datum;
+	@Column(name = "FettfreiesKoerpergewicht")
+	@Setter(value = AccessLevel.NONE)
+	private double fettfreiesKoerpergewicht;
+	@Column(name = "BodyMassIndex")
+	@Setter(value = AccessLevel.NONE)
+	private double bodyMassIndex;
+	@Column(name = "FatFreeMassIndex")
+	@Setter(value = AccessLevel.NONE)
+	private double fatFreeMassIndex;
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
 	@JoinColumn(name = "BenutzerID", nullable = false)
 	private Benutzer benutzer;
 
 	public Koerpermessung(
 		final Primaerschluessel primaerschluessel,
+		final LocalDate datum,
 		final int koerpergroesse,
 		final double koerpergewicht,
 		final double koerperfettAnteil,
-		final double fettfreiesKoerpergewicht,
-		final double subkutanesFett,
-		final double viszeralfett,
-		final double koerperwasser,
-		final double skelettmuskel,
-		final double muskelmasse,
-		final double knochenmasse,
-		final double protein,
-		final int grundumsatz,
 		final int eingenommeneKalorien,
 		final int verbrannteKalorien,
-		final int biologischesAlter,
-		final LocalDate datum,
 		final Benutzer benutzer)
 	{
 		this.primaerschluessel = primaerschluessel;
+		this.datum = datum;
 		this.koerpergroesse = koerpergroesse;
 		this.koerpergewicht = koerpergewicht;
-		bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
-		fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
 		this.koerperfettAnteil = koerperfettAnteil;
-		this.fettfreiesKoerpergewicht = fettfreiesKoerpergewicht;
-		this.subkutanesFett = subkutanesFett;
-		this.viszeralfett = viszeralfett;
-		this.koerperwasser = koerperwasser;
-		this.skelettmuskel = skelettmuskel;
-		this.muskelmasse = muskelmasse;
-		this.knochenmasse = knochenmasse;
-		this.protein = protein;
-		this.grundumsatz = grundumsatz;
 		this.eingenommeneKalorien = eingenommeneKalorien;
 		this.verbrannteKalorien = verbrannteKalorien;
-		this.biologischesAlter = biologischesAlter;
-		this.datum = datum;
+		this.fettfreiesKoerpergewicht = berechneFettfreiesKoerpergewicht(koerpergewicht, koerperfettAnteil);
+		this.bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
+		this.fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
 		this.benutzer = benutzer;
 
 		benutzer.fuegeKoerpermessungHinzu(this);
+	}
+
+	private double berechneFettfreiesKoerpergewicht(final double koerpergewicht, final double koerperfettAnteil)
+	{
+		var fettfreiesKoerpergewicht = koerpergewicht - koerpergewicht * (koerperfettAnteil / 100);
+		return Math.round(fettfreiesKoerpergewicht * 100) / 100.0;
 	}
 
 	private double berechneBmi(final int koerpergroesse, final double koerpergewicht)
@@ -128,21 +101,24 @@ public class Koerpermessung extends Entitaet
 	public void setKoerpergroesse(final int koerpergroesse)
 	{
 		this.koerpergroesse = koerpergroesse;
-		bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
-		fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
+		this.bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
+		this.fettfreiesKoerpergewicht = berechneFettfreiesKoerpergewicht(koerpergewicht, koerperfettAnteil);
+		this.fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
 	}
 
 	public void setKoerpergewicht(final double koerpergewicht)
 	{
 		this.koerpergewicht = koerpergewicht;
-		bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
-		fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
+		this.fettfreiesKoerpergewicht = berechneFettfreiesKoerpergewicht(koerpergewicht, koerperfettAnteil);
+		this.bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
+		this.fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
 	}
 
 	public void setKoerperfettAnteil(final double koerperfettAnteil)
 	{
 		this.koerperfettAnteil = koerperfettAnteil;
-		bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
-		fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
+		this.fettfreiesKoerpergewicht = berechneFettfreiesKoerpergewicht(koerpergewicht, koerperfettAnteil);
+		this.bodyMassIndex = berechneBmi(koerpergroesse, koerpergewicht);
+		this.fatFreeMassIndex = berechneFfmi(koerpergewicht, koerperfettAnteil, koerpergroesse);
 	}
 }
--- src/test/java/de/justinharder/trainharder/model/domain/KoerpermessungSollte.java
+++ src/test/java/de/justinharder/trainharder/model/domain/KoerpermessungSollte.java
@@ -38,44 +38,25 @@ public class KoerpermessungSollte
 		final var id = new Primaerschluessel();
 		final var koerpermessung = new Koerpermessung(
 			id,
+			LocalDate.now(),
 			178,
 			90,
 			25,
-			71,
-			20,
-			11,
-			56,
-			49,
-			67,
-			4,
-			17.5,
-			1900,
 			2500,
 			2900,
-			25,
-			LocalDate.now(),
 			Testdaten.BENUTZER_JUSTIN);
 
 		assertAll(
 			() -> assertThat(koerpermessung.getPrimaerschluessel()).isEqualTo(id),
+			() -> assertThat(koerpermessung.getDatum()).isEqualTo(LocalDate.now()),
 			() -> assertThat(koerpermessung.getKoerpergroesse()).isEqualTo(178),
 			() -> assertThat(koerpermessung.getKoerpergewicht()).isEqualTo(90),
-			() -> assertThat(koerpermessung.getBodyMassIndex()).isEqualTo(28.41),
-			() -> assertThat(koerpermessung.getFatFreeMassIndex()).isEqualTo(21.43),
 			() -> assertThat(koerpermessung.getKoerperfettAnteil()).isEqualTo(25),
-			() -> assertThat(koerpermessung.getFettfreiesKoerpergewicht()).isEqualTo(71),
-			() -> assertThat(koerpermessung.getSubkutanesFett()).isEqualTo(20),
-			() -> assertThat(koerpermessung.getViszeralfett()).isEqualTo(11),
-			() -> assertThat(koerpermessung.getKoerperwasser()).isEqualTo(56),
-			() -> assertThat(koerpermessung.getSkelettmuskel()).isEqualTo(49),
-			() -> assertThat(koerpermessung.getMuskelmasse()).isEqualTo(67),
-			() -> assertThat(koerpermessung.getKnochenmasse()).isEqualTo(4),
-			() -> assertThat(koerpermessung.getProtein()).isEqualTo(17.5),
-			() -> assertThat(koerpermessung.getGrundumsatz()).isEqualTo(1900),
 			() -> assertThat(koerpermessung.getEingenommeneKalorien()).isEqualTo(2500),
 			() -> assertThat(koerpermessung.getVerbrannteKalorien()).isEqualTo(2900),
-			() -> assertThat(koerpermessung.getBiologischesAlter()).isEqualTo(25),
-			() -> assertThat(koerpermessung.getDatum()).isEqualTo(LocalDate.now()),
+			() -> assertThat(koerpermessung.getFettfreiesKoerpergewicht()).isEqualTo(67.5),
+			() -> assertThat(koerpermessung.getBodyMassIndex()).isEqualTo(28.41),
+			() -> assertThat(koerpermessung.getFatFreeMassIndex()).isEqualTo(21.43),
 			() -> assertThat(koerpermessung.getBenutzer()).isEqualTo(Testdaten.BENUTZER_JUSTIN));
 	}
 
@@ -85,24 +66,15 @@ public class KoerpermessungSollte
 	{
 		assertAll(
 			() -> assertThat(sut.getPrimaerschluessel()).isEqualTo(Testdaten.KOERPERMESSUNG_JUSTIN_ID),
+			() -> assertThat(sut.getDatum()).isEqualTo(LocalDate.now()),
 			() -> assertThat(sut.getKoerpergroesse()).isEqualTo(178),
 			() -> assertThat(sut.getKoerpergewicht()).isEqualTo(90),
-			() -> assertThat(sut.getBodyMassIndex()).isEqualTo(28.41),
-			() -> assertThat(sut.getFatFreeMassIndex()).isEqualTo(21.43),
 			() -> assertThat(sut.getKoerperfettAnteil()).isEqualTo(25),
-			() -> assertThat(sut.getFettfreiesKoerpergewicht()).isEqualTo(71),
-			() -> assertThat(sut.getSubkutanesFett()).isEqualTo(20),
-			() -> assertThat(sut.getViszeralfett()).isEqualTo(11),
-			() -> assertThat(sut.getKoerperwasser()).isEqualTo(56),
-			() -> assertThat(sut.getSkelettmuskel()).isEqualTo(49),
-			() -> assertThat(sut.getMuskelmasse()).isEqualTo(67),
-			() -> assertThat(sut.getKnochenmasse()).isEqualTo(4),
-			() -> assertThat(sut.getProtein()).isEqualTo(17.5),
-			() -> assertThat(sut.getGrundumsatz()).isEqualTo(1900),
 			() -> assertThat(sut.getEingenommeneKalorien()).isEqualTo(2500),
 			() -> assertThat(sut.getVerbrannteKalorien()).isEqualTo(2900),
-			() -> assertThat(sut.getBiologischesAlter()).isEqualTo(25),
-			() -> assertThat(sut.getDatum()).isEqualTo(LocalDate.now()),
+			() -> assertThat(sut.getFettfreiesKoerpergewicht()).isEqualTo(67.5),
+			() -> assertThat(sut.getBodyMassIndex()).isEqualTo(28.41),
+			() -> assertThat(sut.getFatFreeMassIndex()).isEqualTo(21.43),
 			() -> assertThat(sut.getBenutzer()).isEqualTo(Testdaten.BENUTZER_JUSTIN));
 	}
 
@@ -113,46 +85,25 @@ public class KoerpermessungSollte
 		final var id = new Primaerschluessel();
 		final var koerpermessung = new Koerpermessung();
 		koerpermessung.setPrimaerschluessel(id);
+		koerpermessung.setDatum(LocalDate.now());
 		koerpermessung.setKoerpergroesse(178);
 		koerpermessung.setKoerpergewicht(90);
-		koerpermessung.setBodyMassIndex(28.41);
-		koerpermessung.setFatFreeMassIndex(21.43);
 		koerpermessung.setKoerperfettAnteil(25);
-		koerpermessung.setFettfreiesKoerpergewicht(71);
-		koerpermessung.setSubkutanesFett(20);
-		koerpermessung.setViszeralfett(11);
-		koerpermessung.setKoerperwasser(56);
-		koerpermessung.setSkelettmuskel(49);
-		koerpermessung.setMuskelmasse(67);
-		koerpermessung.setKnochenmasse(4);
-		koerpermessung.setProtein(17.5);
-		koerpermessung.setGrundumsatz(1900);
 		koerpermessung.setEingenommeneKalorien(2500);
 		koerpermessung.setVerbrannteKalorien(2900);
-		koerpermessung.setBiologischesAlter(25);
-		koerpermessung.setDatum(LocalDate.now());
 		koerpermessung.setBenutzer(Testdaten.BENUTZER_JUSTIN);
 
 		assertAll(
 			() -> assertThat(koerpermessung.getPrimaerschluessel()).isEqualTo(id),
+			() -> assertThat(koerpermessung.getDatum()).isEqualTo(LocalDate.now()),
 			() -> assertThat(koerpermessung.getKoerpergroesse()).isEqualTo(178),
 			() -> assertThat(koerpermessung.getKoerpergewicht()).isEqualTo(90),
-			() -> assertThat(koerpermessung.getBodyMassIndex()).isEqualTo(28.41),
-			() -> assertThat(koerpermessung.getFatFreeMassIndex()).isEqualTo(21.43),
 			() -> assertThat(koerpermessung.getKoerperfettAnteil()).isEqualTo(25),
-			() -> assertThat(koerpermessung.getFettfreiesKoerpergewicht()).isEqualTo(71),
-			() -> assertThat(koerpermessung.getSubkutanesFett()).isEqualTo(20),
-			() -> assertThat(koerpermessung.getViszeralfett()).isEqualTo(11),
-			() -> assertThat(koerpermessung.getKoerperwasser()).isEqualTo(56),
-			() -> assertThat(koerpermessung.getSkelettmuskel()).isEqualTo(49),
-			() -> assertThat(koerpermessung.getMuskelmasse()).isEqualTo(67),
-			() -> assertThat(koerpermessung.getKnochenmasse()).isEqualTo(4),
-			() -> assertThat(koerpermessung.getProtein()).isEqualTo(17.5),
-			() -> assertThat(koerpermessung.getGrundumsatz()).isEqualTo(1900),
 			() -> assertThat(koerpermessung.getEingenommeneKalorien()).isEqualTo(2500),
 			() -> assertThat(koerpermessung.getVerbrannteKalorien()).isEqualTo(2900),
-			() -> assertThat(koerpermessung.getBiologischesAlter()).isEqualTo(25),
-			() -> assertThat(koerpermessung.getDatum()).isEqualTo(LocalDate.now()),
+			() -> assertThat(koerpermessung.getFettfreiesKoerpergewicht()).isEqualTo(67.5),
+			() -> assertThat(koerpermessung.getBodyMassIndex()).isEqualTo(28.41),
+			() -> assertThat(koerpermessung.getFatFreeMassIndex()).isEqualTo(21.43),
 			() -> assertThat(koerpermessung.getBenutzer()).isEqualTo(Testdaten.BENUTZER_JUSTIN));
 	}
 
