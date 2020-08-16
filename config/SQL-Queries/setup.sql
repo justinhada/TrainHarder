06655599e39a5f3e205668640446e25c09d537c7
Justin Harder <justinharder@t-online.de>
2020-08-16 14:46:00 +0200

    Erstelle Vorlage fuer TestDB

--- config/SQL-Queries/setup.sql
+++ config/SQL-Queries/setup.sql
@@ -5,6 +5,13 @@ CREATE USER 'powerlifter'@'localhost' IDENTIFIED BY 'passwort';
 GRANT ALL PRIVILEGES ON `trainharder`.* TO 'powerlifter'@'localhost' IDENTIFIED BY 'passwort';
 FLUSH privileges;
 
+CREATE SCHEMA `trainharderTest` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE USER 'powerlifter'@'%' IDENTIFIED BY 'passwort';
+GRANT ALL PRIVILEGES ON `trainharderTest`.* TO 'powerlifter'@'%' IDENTIFIED BY 'passwort';
+CREATE USER 'powerlifter'@'localhost' IDENTIFIED BY 'passwort';
+GRANT ALL PRIVILEGES ON `trainharderTest`.* TO 'powerlifter'@'localhost' IDENTIFIED BY 'passwort';
+FLUSH privileges;
+
 CREATE TABLE Benutzer
 (
   ID INT(11) NOT NULL AUTO_INCREMENT,
