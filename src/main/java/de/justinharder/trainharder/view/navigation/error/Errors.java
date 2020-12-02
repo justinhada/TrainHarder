cef7bdff78102527abc6b72fb509636df5f5000e
Justin Harder <justinharder@t-online.de>
2020-12-02 08:47:46 +0100

    Entferne ErrorController

--- src/main/java/de/justinharder/trainharder/view/navigation/error/Errors.java
+++
@@ -1,8 +0,0 @@
-package de.justinharder.trainharder.view.navigation.error;
-
-import java.util.HashMap;
-
-public class Errors extends HashMap<String, Exception>
-{
-	private static final long serialVersionUID = 4821143385928446881L;
-}
