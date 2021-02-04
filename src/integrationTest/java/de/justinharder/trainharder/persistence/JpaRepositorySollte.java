3b385cf35e7a4b3ec2b7670271a1ec4e0d1b7294
Justin Harder <justinharder@t-online.de>
2021-02-04 11:17:49 +0100

    Aendere PersistenceUnit

--- src/integrationTest/java/de/justinharder/trainharder/persistence/JpaRepositorySollte.java
+++ src/integrationTest/java/de/justinharder/trainharder/persistence/JpaRepositorySollte.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 abstract class JpaRepositorySollte
 {
-	private static final String PERSISTENCE_UNIT_NAME = "TestRepoPU";
+	private static final String PERSISTENCE_UNIT_NAME = "TestPU";
 
 	@Container
 	private static final MariaDBContainer<?> MARIA_DB_CONTAINER = new MariaDBContainer<>(DockerImageName.parse("mariadb"))
