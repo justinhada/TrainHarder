c7688168a5796daa9f7ad6a232779f7d3c7414cd
Justin Harder <justinharder@t-online.de>
2024-04-22 19:46:07 +0200

    Beginne mit JWT-Authentifizierung im Backend

---
+++ src/main/java/de/justinharder/trainharder/utils/JWTAuthentication.java
@@ -0,0 +1,11 @@
+package de.justinharder.trainharder.utils;
+
+import jakarta.ws.rs.NameBinding;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JWTAuthentication
+{}
