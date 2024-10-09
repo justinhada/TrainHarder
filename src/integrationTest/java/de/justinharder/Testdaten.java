7ad54f42a512dd8f8fee20ed6c93e6d4aaa745ee
Justin Harder <justinharder@t-online.de>
2024-10-09 20:50:34 +0200

    Verschiebe Testdaten

--- src/integrationTest/java/de/justinharder/Testdaten.java
+++ src/integrationTest/java/de/justinharder/Testdaten.java
@@ -2,18 +2,24 @@ package de.justinharder;
 
 import de.justinharder.base.domain.model.attribute.ID;
 import de.justinharder.base.domain.services.dto.pagination.PaginationRequest;
+import de.justinharder.base.domain.services.dto.pagination.PaginationResponse;
 import de.justinharder.dietharder.domain.model.Ziel;
 import de.justinharder.dietharder.domain.model.attribute.KoerperfettAnteil;
 import de.justinharder.dietharder.domain.model.attribute.Koerpergewicht;
 import de.justinharder.trainharder.domain.model.Benutzer;
 import de.justinharder.trainharder.domain.model.attribute.*;
+import de.justinharder.trainharder.domain.services.dto.benutzer.GespeicherterBenutzer;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Testdaten
 {
 	public static final PaginationRequest<?> PAGINATION_REQUEST = new PaginationRequest<>();
+
+	public static final String APPLICATION_JSON_UTF8 = "application/json;charset=UTF-8";
 
 	public static final Benutzer BENUTZER_1 = new Benutzer(
 		new ID("82f4bf94-9f6e-43f7-a6c2-867949e7d4f7"),
@@ -43,6 +49,26 @@ public class Testdaten
 		Geschlecht.WEIBLICH,
 		new Geburtsdatum("1978-07-22"));
 
+	public static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_1 = new GespeicherterBenutzer(
+		"82f4bf94-9f6e-43f7-a6c2-867949e7d4f7",
+		"Justin",
+		"Harder");
+
+	public static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_2 = new GespeicherterBenutzer(
+		"e5e0a05a-471f-41ff-986e-e230f814f4e0",
+		"Nicole",
+		"Harder");
+
+	public static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_3 = new GespeicherterBenutzer(
+		"b073864a-1067-4d23-a007-7b44ea76e95f",
+		"Wolfgang",
+		"Harder");
+
+	public static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_4 = new GespeicherterBenutzer(
+		"2dd931bf-b9f3-441a-8851-e33d1e323827",
+		"Lilia",
+		"Harder");
+
 	public static final Ziel ZIEL_1 = new Ziel(
 		new ID("84d996cf-cd6e-4519-b440-a9d8f1b63809"),
 		new Datum("01.04.2024"),
@@ -56,4 +82,13 @@ public class Testdaten
 		new Koerpergewicht("75"),
 		new KoerperfettAnteil("7"),
 		BENUTZER_1);
+
+	public static final PaginationResponse<GespeicherterBenutzer> PAGINATION_RESPONSE = new PaginationResponse<>(
+		4,
+		List.of(GESPEICHERTER_BENUTZER_1, GESPEICHERTER_BENUTZER_2, GESPEICHERTER_BENUTZER_3, GESPEICHERTER_BENUTZER_4))
+		.setSelf("http://localhost:8080/benutzer?page=1&page_size=10")
+		.setFirst("http://localhost:8080/benutzer?page=1&page_size=10")
+		.setPrev(null)
+		.setNext(null)
+		.setLast("http://localhost:8080/benutzer?page=1&page_size=10");
 }
--- src/integrationTest/java/de/justinharder/trainharder/api/BenutzerRessourceTest.java
+++ src/integrationTest/java/de/justinharder/trainharder/api/BenutzerRessourceTest.java
@@ -9,43 +9,13 @@ import jakarta.ws.rs.core.GenericType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
+import static de.justinharder.Testdaten.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @QuarkusTest
 class BenutzerRessourceTest
 {
-	private static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_1 = new GespeicherterBenutzer(
-		"82f4bf94-9f6e-43f7-a6c2-867949e7d4f7",
-		"Justin",
-		"Harder");
-
-	private static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_2 = new GespeicherterBenutzer(
-		"e5e0a05a-471f-41ff-986e-e230f814f4e0",
-		"Nicole",
-		"Harder");
-
-	private static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_3 = new GespeicherterBenutzer(
-		"b073864a-1067-4d23-a007-7b44ea76e95f",
-		"Wolfgang",
-		"Harder");
-
-	private static final GespeicherterBenutzer GESPEICHERTER_BENUTZER_4 = new GespeicherterBenutzer(
-		"2dd931bf-b9f3-441a-8851-e33d1e323827",
-		"Lilia",
-		"Harder");
-
-	private static final PaginationResponse<GespeicherterBenutzer> PAGINATION_RESPONSE = new PaginationResponse<>(
-		4,
-		List.of(GESPEICHERTER_BENUTZER_1, GESPEICHERTER_BENUTZER_2, GESPEICHERTER_BENUTZER_3, GESPEICHERTER_BENUTZER_4))
-		.setSelf("http://localhost:8080/benutzer?page=1&page_size=10")
-		.setFirst("http://localhost:8080/benutzer?page=1&page_size=10")
-		.setPrev(null)
-		.setNext(null)
-		.setLast("http://localhost:8080/benutzer?page=1&page_size=10");
-
 	@Inject
 	BenutzerRessource sut;
 
